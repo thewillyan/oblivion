8879613ec1e6a2ec886251d8a19a49c5e0e5a811
André Willyan <andrewillyan@tutanota.com>
2024-09-28 22:01:06 -0300

    feat: add basic CLI

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,27 +1,94 @@
 #include "oblivion/ordered_list.hpp"
+#include <fstream>
 #include <iostream>
+#include <istream>
+#include <string>
 
-int main() {
-  std::cout << "oblivion v0.1.0" << std::endl;
+void handle_ops(oblivion::OrderedList &list, std::istream &is,
+                std::ostream &os) {
+  std::string line;
+  while (std::getline(is, line)) {
+    std::string op = line.substr(0, 3);
 
-  oblivion::OrderedList list;
-  list.include(1);
-  list.include(0);
-  list.include(2);
-  list.include(20);
-  list.include(5);
-  list.include(3);
-  list.include(4);
-  list.include(40);
-  list.include(6);
+    std::string rest;
+    if (line.size() > 4) {
+      rest = line.substr(4);
+    }
 
-  std::cout << list << std::endl;
+    if (op == "INC") {
+      list.include(std::stoi(rest));
+    } else if (op == "REM") {
+      list.erase(std::stoi(rest));
+    } else if (op == "SUC") {
+      std::optional<int> suc = list.successor(std::stoi(rest));
+      if (suc.has_value()) {
+        os << *suc << std::endl;
+      }
+    } else if (op == "IMP") {
+      os << list << std::endl;
+    } else if (op == "EXT") {
+      break;
+    }
+  }
+}
 
-  list.erase(4);
-  list.erase(1);
-  list.erase(5);
+void print_help() {
+  const auto HELP_MSG = "oblivion-cli v0.1.0\n"
+                        "Usage: oblivion-cli [option] [filename]\n\n"
+                        "Options:\n"
+                        "-f\tReceive operations from a file\n"
+                        "-i\tReceive operations iteratively\n"
+                        "-h\tPrint this help menssage\n\n"
+                        "Operations: [INC <int>|REM <int>|SUC <int>|IMP|EXT]\n";
+  std::cout << HELP_MSG;
+}
 
-  std::cout << list << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc < 2 || argc > 3) {
+    std::cout << "Invalid number of arguments. See -h for help." << std::endl;
+    return 1;
+  }
+
+  std::string opt = argv[1];
+  if (opt == "-h") {
+    if (argc != 2) {
+      std::cout << "Invalid argument to -h." << std::endl;
+      return 1;
+    }
+    print_help();
+  } else if (opt == "-i") {
+    if (argc != 2) {
+      std::cout << "Invalid argument to -i. See -h for help." << std::endl;
+      return 1;
+    }
+    oblivion::OrderedList list{};
+    handle_ops(list, std::cin, std::cout);
+  } else if (opt == "-f") {
+    if (argc != 3) {
+      std::cout << "Invalid argument to -f. See -h for help." << std::endl;
+      return 1;
+    }
+
+    std::string filename = argv[2];
+    std::ifstream in_file(filename);
+    if (!in_file) {
+      std::cout << "Failed to open the file '" << filename << "'." << std::endl;
+      return 1;
+    }
+
+    std::string out_filename = filename + ".out";
+    std::ofstream out_file(out_filename);
+    if (!out_file) {
+      std::cout << "Failed to open the file '" << out_filename << "'."
+                << std::endl;
+      return 1;
+    }
+    oblivion::OrderedList list{};
+    handle_ops(list, in_file, out_file);
+  } else {
+    std::cout << "Invalid option '" << opt << "'!" << std::endl;
+    return 1;
+  }
 
   return 0;
 }
